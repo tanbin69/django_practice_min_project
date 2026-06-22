79c716e15b863add6e34c513c0d6e16b9daeac45
tanbin69 <tanbinahamed226@gmail.com>
2026-06-22 22:17:58 +0600

    created views,urls,template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import post
 
 # Create your views here.
+def post_list(request):
+    all_posts = post.objects.all()
+
+    context = {
+        'posts' : all_posts
+    }
+    return render(request,'blog/post_list.html',context)
